111dbce773a7518148700e0e0b1892f2e5df116c
Nick Foden <nickfoden@gmail.com>
2017-04-22 15:47:30 -0400

    adding routes

--- server.js
+++ server.js
@@ -3,11 +3,33 @@
 var express = require('express');
 var app = express();
 
-app.use(express.static('public'));
-app.listen(process.env.PORT || 8080);
-
 exports.app = app;
 
-app.get('/end', function (req, res){
-	res.render('/end')
+app.get('/', function (req, res){
+	res.send('index')
 });
+
+app.get('/summary', function (req, res){
+	res.send('summary')
+});
+
+app.get('/end', function (req, res){
+	res.send('end')
+});
+
+
+app.get('/new-card', function (req, res){
+	res.send('new-card')
+});
+
+app.get('/sign-up', function (req, res){
+	res.send('sign-up')
+});
+
+app.get('/start', function (req, res){
+	res.send('start')
+});
+
+
+app.use(express.static('public'));
+app.listen(process.env.PORT || 8080);
