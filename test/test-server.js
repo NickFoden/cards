18a2dff9c50c4c2365753f7f0571445e2d7b8c42
Nick Foden <nickfoden@gmail.com>
2017-04-22 14:33:07 -0400

    Added routes and tests for those routes

--- test/test-server.js
+++ test/test-server.js
@@ -9,7 +9,7 @@ chai.use(chaiHttp);
 
 
 describe('Cards Test Index Page', function(){
-	it('should be alive', function(){
+	it('index should be alive', function(){
 		chai.request(app)
 		.get('/')
 		.end(function(err, res){
@@ -18,4 +18,56 @@ describe('Cards Test Index Page', function(){
 			done();
 		});
 	});
+
+	it('sign up page exists', function(){
+		chai.request(app)
+		.get('/sign-up')
+		.end(function(err, res){
+			res.should.have.status(200);
+			res.should.be.html;
+			done();
+		});
+	});
+
+	it('start page exists', function(){
+		chai.request(app)
+		.get('/start')
+		.end(function(err, res){
+			res.should.have.status(200);
+			res.should.be.html;
+			done();
+		});
+	});
+
+	it('new card page is active', function(){
+		chai.request(app)
+		.get('/new-card')
+		.end(function(err, res){
+			res.should.have.status(200);
+			res.should.be.html;
+			done();
+		});
+	});
+
+	it('sumary page exists', function(){
+		chai.request(app)
+		.get('/summary')
+		.end(function(err, res){
+			res.should.have.status(200);
+			res.should.be.html;
+			done();
+		});
+	});
+
+	it('end page exists', function(){
+		chai.request(app)
+		.get('/end')
+		.end(function(err, res){
+			res.should.have.status(200);
+			res.should.be.html;
+			done();
+		});
+	});
 });
+
+
