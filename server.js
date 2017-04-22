1d8a8055f6a4ed11be4e28833af20f6ebff053e2
Nick Foden <nickfoden@gmail.com>
2017-04-22 12:17:48 -0400

    Added simple test before adding Travis

--- server.js
+++ server.js
@@ -1,4 +1,45 @@
-var express = require('express');
-var app = express();
+const express = require('express');
+const app = express();
 app.use(express.static('public'));
-app.listen(process.env.PORT || 8080);
+
+
+const {PORT, DATABASE_URL} = require('./config');
+const mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise;
+
+let server;
+
+function runServer(databaseUrl=DATABASE_URL, port=PORT) {
+  return new Promise((resolve, reject) => {
+    mongoose.connect(databaseUrl, err => {
+      if (err) {
+        return reject(err);
+      }
+      server = app.listen(port, () => {
+        console.log(`Your app is listening on port ${port}`);
+        resolve();
+      })
+      .on('error', err => {
+        mongoose.disconnect();
+        reject(err);
+      });
+    });
+  });
+}
+
+function closeServer() {
+  return mongoose.disconnect().then(() => {
+     return new Promise((resolve, reject) => {
+       console.log('Closing server');
+       server.close(err => {
+           if (err) {
+               return reject(err);
+           }
+           resolve();
+       });
+     });
+  });
+}
+
+module.exports = {runServer, app, closeServer};
--- test/test-server.js
+++ test/test-server.js
@@ -1,3 +1,34 @@
 const chai = require('chai');
 const chaiHttp = require('chai-http');
 const should = chai.should();
+const {PORT, DATABASE_URL} = require('../config');
+
+chai.use(chaiHttp);
+
+const expect = require('chai').expect;
+
+const {closeServer, runServer, app} =  require('../server');
+
+
+describe('Cards Test', function(){
+	before(function(){
+		return runServer(DATABASE_URL);
+	});
+	after(function(){
+		return closeServer();
+	});
+
+	describe('Check Server', function(){
+		it('should see if server is running', function(){
+		chai.request('/')
+		.get('/')
+		.then(res => {
+			if (res.status >= 200) {
+				return res;
+				}
+			})
+		});
+	});
+
+
+});
