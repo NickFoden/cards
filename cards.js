865a49d96c570da6a7d5c327d5fc9a5ad79ba345
Nick Foden <nickfoden@gmail.com>
2017-04-23 00:17:28 -0400

    added cards file

---
+++ cards.js
@@ -0,0 +1,16 @@
+var cards_js = {
+	"cards": [
+        {
+            "id": "1",
+            "card_q": "const",
+            "card_a": "Declares a read-only named constant"
+            "reference":"https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Grammar_and_types#Basics"
+        },
+        {
+        	"id":"2",
+        	"card_q":"var",
+        	"card_a":"Declares a variable, optionally initializing it to a value.",
+        	"reference":"https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Grammar_and_types#Basics"
+        }
+ ]
+};
