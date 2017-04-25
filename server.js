c9f1fdf11b5ab6d843a17e9e5d59f37d9795da3d
Nick Foden <nickfoden@gmail.com>
2017-04-24 22:23:35 -0400

    removed all get routes

--- server.js
+++ server.js
@@ -7,29 +7,4 @@ var path = require('path');
 
 exports.app = app;
 
-app.get('/', function (req, res){
-	res.sendFile(path.join(__dirnname + '/public/index.html'));
-});
-
-app.get('/summary', function (req, res){
-	res.sendFile(path.join(__dirname + '/public/summary.html'));
-});
-
-app.get('/end-of-cards', function (req, res){
-	res.sendFile(path.join(__dirname + '/public/end-of-cards.html'));
-});
-
-app.get('/new-card', function (req, res){
-	res.sendFile(path.join(__dirname + '/public/new-card.html'));
-});
-
-app.get('/sign-up', function (req, res){
-	res.sendFile(path.join(__dirname + '/public/sign-up.html'));
-});
-
-app.get('/start', function (req, res){
-	res.sendFile(path.join(__dirname + '/public/start.html'));
-});
-
-
 app.listen(process.env.PORT || 8080);
