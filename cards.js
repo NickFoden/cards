82c436cbc68277eb3348ee0e52ac847eb0ddbcc8
Nick Foden <nickfoden@gmail.com>
2017-04-25 21:39:15 -0400

    Added index to select cards

--- cards.js
+++ cards.js
@@ -1,35 +1,30 @@
-var cards_js = {
-	"cards": [
+var cards_js = [
         {
-            "id": "1",
-            "card_q": "const",
-            "card_a": "Declares a read-only named constant"
+            "id":"1",
+            "question": "const",
+            "answer": "Declares a read-only named constant",
             "reference":"https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Grammar_and_types#Basics"
         },
         {
         	"id":"2",
-        	"card_q":"var",
-        	"card_a":"Declares a variable, optionally initializing it to a value.",
+        	"question":"var",
+        	"answer":"Declares a variable, optionally initializing it to a value.",
         	"reference":"https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Grammar_and_types#Basics"
         }
- ]
-};
+ ];
 
-function displayCard(data){
+function displayCardQuestion(data){
 	resultElement += `<div class="card">`;
-	resultElement += `<h2>${data.card_q}</h2>`;
+	resultElement += `<h2>${data.question}</h2>`;
 	resultElement += `</div>`
 
 	$('#display-card').append(resultElement);
 };
 
+
 let i = 1;
 
-/*function currentCard(cards_js){
-	cards_js.cards[i];
-};*/
-
-var currentCard = cards_js.cards[i];
+var currentCard = cards_js[i];
 
 function displayStart(start){
 	$("#container").removeClass("hidden");
@@ -37,7 +32,7 @@ function displayStart(start){
 
 $("#start-button").on('click', function(){
    displayStart();
-   displayCard(currentCard);
+   displayCardQuestion(currentCard);
 };
 
 
