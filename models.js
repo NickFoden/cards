8e2c4d7698ced59590042471510aec1171329a31
Nick Foden <nickfoden@gmail.com>
2017-05-01 19:54:35 -0400

    Added models file

---
+++ models.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+
+const cardSchema = mongoose.Schema({
+		//"id":"",
+         "question": {type: String, required: true},
+         "answer": {type: String, required: true},
+         "example": {type: String, required: true},
+         "reference": {type: String, required: true}
+});
