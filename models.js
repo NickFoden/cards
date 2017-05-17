6549722ee46158179f4546de5a101399e1a62ffd
Nick Foden <nickfoden@gmail.com>
2017-05-17 11:34:32 -0400

    removed parentheses

--- models.js
+++ models.js
@@ -17,7 +17,7 @@ const userSchema = mongoose.Schema({
          "password": {type: String, required: true},
          "email": {type: String, required: true},
          "name": {type: String}
-     }, 
+     } 
 );
 
 const Card = mongoose.model('Card', cardSchema);
