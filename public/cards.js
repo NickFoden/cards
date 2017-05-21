32e9e9f102bfc1f502d34d722c890a405e1fd0ca
Nick Foden <nickfoden@gmail.com>
2017-05-21 12:02:48 -0400

    Added button styling and changed some text

--- public/cards.js
+++ public/cards.js
@@ -76,7 +76,7 @@ function displayCardAnswer(data){
 function displayEnd(data){
   let resultElement = `<div class="end-card">You have reached the end</div>
                         <button id="start-over" onclick="location.href='start.html'">Start over</button>
-                        <button id="index" onclick="location.href='index.html'">Index</button>`;
+                        <button id="index" onclick="location.href='summary.html'">Index of Cards</button>`;
 
    $('#display').html(resultElement);  
 };
