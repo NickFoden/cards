c84115db69f0ea687694d211340aa71ea3e0b057
Nick Foden <nickfoden@gmail.com>
2017-05-31 22:00:26 -0400

    Adding serialize passport file

--- login.js
+++ login.js
@@ -1,29 +1,36 @@
-// passport/login.js
-passport.use('login', new LocalStrategy({
-    passReqToCallback : true
-  },
-  function(req, email, password, done) { 
-    // check in mongo if a user with username exists or not
-    User.findOne({ 'email' :  email }, 
-      function(err, user) {
-        // In case of any error, return using the done method
-        if (err)
-          return done(err);
-        // Username does not exist, log error & redirect back
-        if (!user){
-          console.log('User Not Found with email '+email);
-          return done(null, false, 
-                req.flash('message', 'Email Not found.'));                 
-        }
-        // User exists but wrong password, log the error 
-        if (!isValidPassword(email, password)){
-          console.log('Invalid Password');
-          return done(null, false, 
-              req.flash('message', 'Invalid Password'));
-        }
-        // User and password both match, return user from 
-        // done method which will be treated like success
-        return done(null, user);
-      }
+const LocalStrategy   = require('passport-local').Strategy;
+const {User} = require('./models.js');
+const bcrypt = require('bcryptjs');
+
+
+module.exports = function(passport){
+
+  passport.use('login', new LocalStrategy({
+            passReqToCallback : true
+        },
+        function(req, email, password, done) { 
+            User.findOne({ 'email' :  email }, 
+                function(err, user) {
+                    if (err)
+                        return done(err);
+                    if (!user){
+                        console.log('User Not Found with username '+username);
+                        return done(null, false, req.flash('message', 'User Not found.'));                 
+                    }
+                    if (!isValidPassword(user, password)){
+                        console.log('Invalid Password');
+                        return done(null, false, req.flash('message', 'Invalid Password')); // redirect back to login page
+                    }
+                    return done(null, user);
+                }
+            );
+
+        })
     );
-}));
+
+
+    var isValidPassword = function(user, password){
+        return bCrypt.compareSync(password, user.password);
+    }
+    
+}
